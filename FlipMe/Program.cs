9ab328ad04167c5e0b0f6a3879fd6d268601ebda
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-05-04 11:38:12 -0500

    Add project files.

---
+++ FlipMe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.DirectoryServices.AccountManagement;
+
+using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
+{
+    UserPrincipal user = UserPrincipal.FindByIdentity(context, "admin");
+
+    if (user != null)
+    {
+        user.PasswordNeverExpires = true;
+        user.Save();
+    }
+}
