446bb260edd1f05da1830ec05bdb10e8b52441c8
0xAnthonyW <123592619+0xAnthonyW@users.noreply.github.com>
2023-05-04 16:26:28 -0500

    small update

--- FlipMe/Program.cs
+++ FlipMe/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System.DirectoryServices.AccountManagement;
 
+string admin = "admin";
+
 using (PrincipalContext context = new PrincipalContext(ContextType.Machine))
 {
-    UserPrincipal user = UserPrincipal.FindByIdentity(context, "admin");
+    UserPrincipal user = UserPrincipal.FindByIdentity(context, admin);
 
     if (user != null)
     {
         user.PasswordNeverExpires = true;
         user.Save();
+    }
+    else
+    {
+        Console.WriteLine("User not found");
     }
 }
